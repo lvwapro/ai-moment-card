5028fa567537a6e852ddd59ca15c32d4143c40e5
huangct <huangct@akulaku.com>
2025-12-01 11:43:47 +0800

    修改Android配置: 应用名称改为'迹见文案'，bundle ID改为com.qualrb.aiPoetry

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.qualrb.aiPoetryCard"
+    namespace = "com.qualrb.aiPoetry"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.qualrb.aiPoetryCard"
+        applicationId = "com.qualrb.aiPoetry"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24  // Required by purchases_ui_flutter and other plugins
